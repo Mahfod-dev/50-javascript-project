eb66568a01176531897bb32d17b16344e470881d
Mahfod <mahfod.dev@gmail.com>
2022-07-02 22:18:37 +0200

    searchHidden

---
+++ searchHidden/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn')
+const container = document.querySelector('.search')
+
+btn.addEventListener('click',()=>{
+
+container.classList.toggle('active')
+input.focus()
+
+})
